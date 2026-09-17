cbc71a73269311c2765839d4770e52e805c89860
Prasanna <84990734+prasanna2001t@users.noreply.github.com>
2026-09-17 12:28:39 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 //          URL here. Example:
 //  const API_BASE = 'https://abxyz.execute-api.ap-south-1.amazonaws.com/prod';
 // ─────────────────────────────────────────────────────────────
-const API_BASE = 'https://xf5vzpgym7.execute-api.ap-south-1.amazonaws.com/default';
+const API_BASE = 'https://zcv9k3zonf.execute-api.ap-south-1.amazonaws.com/Def';
