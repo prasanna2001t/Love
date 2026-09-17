8e8433953c8b8a7679873c60adf0751ac63dc28f
Prasanna <84990734+prasanna2001t@users.noreply.github.com>
2026-09-17 11:26:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 //          URL here. Example:
 //  const API_BASE = 'https://abc123xyz.execute-api.ap-south-1.amazonaws.com/prod';
 // ─────────────────────────────────────────────────────────────
-const API_BASE = 'REPLACE_WITH_YOUR_API_GATEWAY_URL';
+const API_BASE = 'https://xf5vzpgym7.execute-api.ap-south-1.amazonaws.com/default/Love1';
