909def39380ee488d223462ba6de54eb9bd2efa1
Prasanna <84990734+prasanna2001t@users.noreply.github.com>
2026-09-17 12:16:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // ─────────────────────────────────────────────────────────────
 //  STEP 1: After you deploy the Lambda, paste your API Gateway
 //          URL here. Example:
-//  const API_BASE = 'https://abc123xyz.execute-api.ap-south-1.amazonaws.com/prod';
+//  const API_BASE = 'https://abxyz.execute-api.ap-south-1.amazonaws.com/prod';
 // ─────────────────────────────────────────────────────────────
-const API_BASE = 'https://xf5vzpgym7.execute-api.ap-south-1.amazonaws.com';
+const API_BASE = 'https://xf5vzpgym7.execute-api.ap-south-1.amazonaws.com/default';
